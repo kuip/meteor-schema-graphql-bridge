eb9c2cc93f5a2b2ba099f582af36c97625f0e569
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-07-18 00:37:16 +0300

    added git

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'kuip:schema-graphql-bridge',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Simple Schema - GraphQL Schema Bridge',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/kuip/meteor-schema-graphql-bridge.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
